22f3d4f76c9db83fbb0c0614325dc3668274247f
Joshua Zahner <joshuazahner@gmail.com>
2019-04-09 00:06:02 -0400

    Updated documentation of database select methods

--- server/src/types.py
+++ server/src/types.py
@@ -133,34 +133,38 @@ class HazardInfoFilter:
 
 def get_info_by_hazard(hazard_type: HazardType):
 	"""
-	Returns a list of HazardSummaryInfo
-	[HazardSummaryInfo]
+	Returns a list of Hazard
+	:returns [Hazard]
 	"""
 	pass
 
-def get_satellites_by_hazard(hazard_id: str, hazard_type: HazardType):
+def get_satellites_by_hazard(hazard_id: str):
 	"""
 	Returns a list of Satellite
-	[Satellite]
+	:returns [Satellite]
 	"""
 	pass
 
 def get_hazard_data_by_hazard_id(hazard_id: str, filter: HazardInfoFilter):
 	"""
-	Returns hazards by id filtered by satellite, image type, date range, and num images
-	:returns HazardSummaryInfo, [Image]
+	Returns hazards by id filtered by satellite, image type, date range, and num images.
+	This should constitute a multiple table lookup, where first, the data for the provided
+	`hazard_id` is pulled from the `hazards` table, then the images associated with the
+	`hazard_id` are pulled from the `images` table.
+	:returns Hazard, [Image]
 	"""
 	pass
 
 
 
 """
-All datbase insertion methods should take care to do the following:
+All database insertion methods should take care to do the following:
 	1) Connect appropriately to the database
 	2) Validate that the data sent to the method is valid for insertion
 	3) Check to make sure that data is not added to the database multiple times
-	4) Disconnect appropriately from the database
-	5) Return a SUCCESS or FAILURE to the user
+	4) Insert the data as necesarry
+	5) Disconnect appropriately from the database
+	6) Return a SUCCESS or FAILURE to the user
 """
 	
 def create_new_hazard(hazard: Hazard):
