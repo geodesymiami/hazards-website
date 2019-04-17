25579c1e68255b6827df81a89cec376d330604d3
Evan Miller <miller.evan815@gmail.com>
2019-04-17 16:12:42 -0400

    Added image carousel modal popup

--- app/src/ImagesView/ImagesViewComponent.js
+++ app/src/ImagesView/ImagesViewComponent.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react'
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Modal, Container, Image } from 'react-bootstrap';
 
 export default class ImagesViewComponent extends Component {
+    constructor(...args) {
+        super(...args)
+        this.state = {modal: false, index: 0, direction: null}
+    }
     render() {
-        return (
-            <Carousel>
+        return <Container>
+            <Carousel interval={null} 
+            activeIndex={this.state.index} 
+            direction={this.state.direction} 
+            onSelect={(index, direction) => this.setState({index, direction})}
+            >
                 {
-                    this.props.images.map(n => (
-                        <Carousel.Item>
-                            <img src={n['full_image_url']}></img>
+                    this.props.images.map((n, i) => (
+                        <Carousel.Item 
+                        key={i} 
+                        onClick={() => this.setState({modal: true, url: n['full_image_url']})}
+                        style={{cursor:'pointer'}}
+                        >
+                            <Image src={n['full_image_url']} fluid></Image>
                             <Carousel.Caption>
                                 <h3>{n.date.substring(0, 2)}/{n.date.substring(2, 4)}/{n.date.substring(4, 8)}</h3>
                             </Carousel.Caption>
@@ -16,6 +28,11 @@ export default class ImagesViewComponent extends Component {
                     ))
                 }
             </Carousel>
-        )
+            <Modal show={this.state.modal} size="lg" centered onHide={() => this.setState({modal: false})}>
+                <Modal.Body>
+                    <Image src={this.state.url} fluid></Image>
+                </Modal.Body>
+            </Modal>
+        </Container>
     }
 }
