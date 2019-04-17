79a50de864cf39f06d10a70e94454b5d90f5b0bc
Joshua Zahner <joshuazahner@gmail.com>
2019-04-17 11:17:08 -0400

    Added blank python files for DT to flesh out

---
+++ server/test/preprocessing/image_saveing.py
@@ -0,0 +1,16 @@
+"""
+
+    Should include the following functions:
+
+    - save_image_local(image, output_path)
+    - save_images_local(image, output_paths)
+    - save_image_S3(image, output_path)
+    - save_images_S3(image, output_paths)
+
+"""
+
+if __name__ == "__main__":
+    """
+    Please provide an example of how to use your functions. These test cases should
+    work in general (minus the specification of the input files and output paths).
+    """
