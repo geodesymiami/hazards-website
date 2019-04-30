8c0719dddef04aa7bc55c54e5fde279bf505bdbb
Ty Feng <yingtaif@gmail.com>
2019-04-30 14:52:05 -0400

    Update MapViewComponent.js

--- app/src/MapView/MapViewComponent.js
+++ app/src/MapView/MapViewComponent.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './MapViewComponent.css';
 import {  Map, Marker, Popup } from 'react-leaflet';
+import ReactDOM from 'react-dom';
 
 export default class MapViewComponent extends Component {
+    constructor(){
+	super()
+	this.state = {
+	    lat: 25.5,
+	    lng: -80.5,
+	    zoom: 2
+	}
+    }
+
     render() {
-        return (
+	
+        const position = [this.state.lat, this.state.lng];
+	return (
             <div className="MapViewComponent">
-                <h1>Map View</h1>
+	    <h1>Map View</h1>
+      	    <Map center = {position} zoom = {this.state.zoom}>
+	    <Marker position = {position}>
+	    <Popup>Popup 
+	    <br/> More information... 
+	    </Popup>
+	    </Marker>
+	    </Map>
+	       
             </div>
-        )
+		);
     }
 }
+
+ReactDOM.render(<MapViewComponent />, document.getElementById('root'))
