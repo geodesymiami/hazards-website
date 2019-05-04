6c7a131b47f27197af120af83876c167e3e5e1b5
Ty Feng <yingtaif@gmail.com>
2019-05-04 14:48:30 -0400

    Update MapViewComponent.js

--- app/src/MapView/MapViewComponent.js
+++ app/src/MapView/MapViewComponent.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import './MapViewComponent.css';
-import {  Map, Marker, Popup } from 'react-leaflet';
-import ReactDOM from 'react-dom';
-
-export default class MapViewComponent extends Component {
-    constructor(){
-	super()
-	this.state = {
-	    lat: 25.5, // will pass actual variables
+import React, { Component } from 'react'
+import './MapViewComponent.css'
+import {  Map, Marker, TileLayer, Popup } from 'react-leaflet'
+ 
+    type State ={
+    lat: number,
+    lng: number,
+    zoom: number,
+}
+    export default class MapViewComponent extends Component<{},State> {
+	constructor(){
+	 super()
+	 this.state = {
+	    lat: 25.5,
 	    lng: -80.5,
 	    zoom: 2
 	}
-    }
+	}
 
     render() {
 	
         const position = [this.state.lat, this.state.lng];
 	return (
-            <div className="MapViewComponent">
+            <div className='MapViewComponent'>
 	    <h1>Map View</h1>
       	    <Map center = {position} zoom = {this.state.zoom}>
+	    <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 	    <Marker position = {position}>
 	    <Popup>Popup 
 	    <br/> More information... 
@@ -27,9 +33,7 @@ export default class MapViewComponent extends Component {
 	    </Marker>
 	    </Map>
 	       
-            </div>
-		);
+        </div>
+		)
     }
-}
-
-// ReactDOM.render(<MapViewComponent />, document.getElementById('root'))
+ }
