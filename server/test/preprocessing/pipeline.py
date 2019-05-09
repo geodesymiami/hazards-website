0c9522b94f262c5ad67677f5cacbe71ad19586bd
Joshua Zahner <joshuazahner@gmail.com>
2019-05-09 14:54:37 -0500

    pipeline.py now pulls list of images less than a day old from S3 bucket to run processing on

--- server/test/preprocessing/pipeline.py
+++ server/test/preprocessing/pipeline.py
@@ -1,11 +1,31 @@
 import rasterio
 import rasterio.plot as plot
-from random import randint
+import boto3
+from datetime import timedelta, datetime, timezone
+import time
 
 from server.src.db.database import Database
 import server.test.preprocessing.summary as summary
 import server.test.preprocessing.image_manipulation as image
+import server.src.config as config
 from server.src.types import *
+
+
+def get_list_of_images():
+    ACCESS_KEY = config.get_config_var("aws_s3", "access_key")
+    SECRET_KEY = config.get_config_var("aws_s3", "secret_key")
+    BUCKET_NAME = config.get_config_var("aws_s3", "bucket_name")
+
+    s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+    bucket = s3.Bucket(BUCKET_NAME)
+    objects = list(bucket.objects.all())
+
+    today = datetime.now(timezone.utc)
+    delta = timedelta(days=-1)
+
+    recent = list(filter(lambda o: o.last_modified.replace(tzinfo=timezone.utc).astimezone(tz=None) - today > delta, objects))
+
+    return recent
 
 if __name__ == "__main__":
 
@@ -22,44 +42,50 @@ if __name__ == "__main__":
         10. Push new object references to database
     """
 
-    file_path = "hazards-website/Ortho_20170101_backscatter.tif"
-    full_path = "/Users/joshua/Desktop/{}_full.jpg"
-    mod_path = "/Users/joshua/Desktop/{}_mod.jpg"
+    images = get_list_of_images()
 
-    # 1. Read in image file
-    data = rasterio.open("s3://{}".format(file_path))
-    band = data.read(1)
-    img = plot.show(band)
+    for im in images:
 
-    # 2. Pull summary statistics from file
-    haz_id, haz_name, sat_name, sat_dir, img_type, img_date, center = summary.pull_summary_data("/vsis3/{}".format(file_path))
+        file_path = "{}/{}".format(im.bucket_name, im.key)
+        full_path = "/Users/joshua/Desktop/{}_full.jpg"
+        mod_path = "/Users/joshua/Desktop/{}_mod.jpg"
 
-    img.get_figure().savefig(full_path.format(img_date), dpi=300)
+        # 1. Read in image file
+        data = rasterio.open("s3://{}".format(file_path))
+        band = data.read(1)
+        img = plot.show(band)
 
-    # 3. Compress image
-    compressed = image.compress_image(full_path.format(img_date), compression_amount=0.3)
+        # 2. Pull summary statistics from file
+        haz_id, haz_name, sat_name, sat_dir, img_type, img_date, center = summary.pull_summary_data("/vsis3/{}".format(file_path))
 
-    # 4 - 5. Pad image and add date on image
-    text_image = image.add_text_to_image(compressed, img_date)
+        img.get_figure().savefig(full_path.format(img_date), dpi=300)
 
-    # 6. Save image locally
-    text_image.save(mod_path.format(img_date))
+        # 3. Compress image
+        compressed = image.compress_image(full_path.format(img_date), compression_amount=0.3)
 
-    # hazard = Hazard(haz_id, haz_name, HazardType.VOLCANO, Location(LatLong(center[0], center[1])), Date(img_date))
-    # sat_id = SatelliteEnum.from_string(sat_name)
-    # satellite = Satellite(sat_id, sat_dir)
-    # image = Image(str(randint(1, 10000000)),
-    #               haz_id,
-    #               satellite,
-    #               ImageType.from_string(img_type),
-    #               Date(img_date),
-    #               ImageURL(mod_path.format(img_date)),
-    #               ImageURL(full_path.format(img_date)),
-    #               ImageURL(file_path))
-    #
-    # db = Database()
-    # db.create_new_hazard(hazard)
-    # db.create_new_satellite(satellite)
-    # db.create_new_image(image)
-    # db.close()
+        # 4 - 5. Pad image and add date on image
+        text_image = image.add_text_to_image(compressed, img_date)
+
+        # 6. Save image locally
+        text_image.save(mod_path.format(img_date))
+
+        time.sleep(10)
+
+        # hazard = Hazard(haz_id, haz_name, HazardType.VOLCANO, Location(LatLong(center[0], center[1])), Date(img_date))
+        # sat_id = SatelliteEnum.from_string(sat_name)
+        # satellite = Satellite(sat_id, sat_dir)
+        # image = Image(str(randint(1, 10000000)),
+        #               haz_id,
+        #               satellite,
+        #               ImageType.from_string(img_type),
+        #               Date(img_date),
+        #               ImageURL(mod_path.format(img_date)),
+        #               ImageURL(full_path.format(img_date)),
+        #               ImageURL(file_path))
+        #
+        # db = Database()
+        # db.create_new_hazard(hazard)
+        # db.create_new_satellite(satellite)
+        # db.create_new_image(image)
+        # db.close()
 
