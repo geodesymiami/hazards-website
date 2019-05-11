6596be756a55621d649c30e5a418e54186513ce7
Joshua Zahner <joshuazahner@gmail.com>
2019-05-11 13:50:46 -0500

    Modified to deal with .tif files hosted on AWS S3 rather than just local files

--- server/test/preprocessing/pipeline.py
+++ server/test/preprocessing/pipeline.py
@@ -3,10 +3,13 @@ import rasterio.plot as plot
 import boto3
 from datetime import timedelta, datetime, timezone
 import time
+import os
+from random import randint
 
 from server.src.db.database import Database
 import server.test.preprocessing.summary as summary
-import server.test.preprocessing.image_manipulation as image
+import server.test.preprocessing.image_manipulation as immanip
+import server.test.preprocessing.image_saveing as save
 import server.src.config as config
 from server.src.types import *
 
@@ -23,9 +26,12 @@ def get_list_of_images():
     today = datetime.now(timezone.utc)
     delta = timedelta(days=-1)
 
-    recent = list(filter(lambda o: o.last_modified.replace(tzinfo=timezone.utc).astimezone(tz=None) - today > delta, objects))
-
+    recent = list(
+        filter(lambda o: o.last_modified.replace(tzinfo=timezone.utc).astimezone(tz=None) - today > delta, objects))
+    recent = list(filter(lambda f: "tif" in os.path.splitext(f.key)[1], recent))
+    recent = list(filter(lambda f: "/" not in f.key, recent))
     return recent
+
 
 if __name__ == "__main__":
 
@@ -43,33 +49,48 @@ if __name__ == "__main__":
     """
 
     images = get_list_of_images()
-
+    print(images)
     for im in images:
 
         file_path = "{}/{}".format(im.bucket_name, im.key)
-        full_path = "/Users/joshua/Desktop/{}_full.jpg"
-        mod_path = "/Users/joshua/Desktop/{}_mod.jpg"
+        full_path = "{}_full.jpg"
+        mod_path = "{}_mod.jpg"
+        aws_path = "{}/{}/{}/{}"
+        try:
+            haz_id, haz_name, sat_name, sat_dir, img_type, img_date, center = summary.pull_summary_data(
+                "/vsis3/{}".format(file_path))
+            sat_id = SatelliteEnum.from_string(sat_name)
+        except:
+            continue
+
+        print(haz_id, haz_name, sat_id, sat_name, sat_dir, img_type, img_date)
+
+        aws_path = aws_path.format(haz_id, sat_id, img_type, img_date)
+        full_path = full_path.format(img_date)
+        mod_path = mod_path.format(img_date)
 
         # 1. Read in image file
-        data = rasterio.open("s3://{}".format(file_path))
-        band = data.read(1)
-        img = plot.show(band)
-
-        # 2. Pull summary statistics from file
-        haz_id, haz_name, sat_name, sat_dir, img_type, img_date, center = summary.pull_summary_data("/vsis3/{}".format(file_path))
-
-        img.get_figure().savefig(full_path.format(img_date), dpi=300)
+        with rasterio.open("s3://{}".format(file_path)) as data:
+            band = data.read(1)
+            img = plot.show(band)
+            img.get_figure().savefig(full_path, dpi=300)
 
         # 3. Compress image
-        compressed = image.compress_image(full_path.format(img_date), compression_amount=0.3)
+        compressed = immanip.compress_image(full_path, compression_amount=0.3)
 
         # 4 - 5. Pad image and add date on image
-        text_image = image.add_text_to_image(compressed, img_date)
+        text_image = immanip.add_text_to_image(compressed, img_date)
 
         # 6. Save image locally
         text_image.save(mod_path.format(img_date))
+        mod_path_aws = save.save_image_s3(mod_path, "{}/{}".format(aws_path, mod_path))
+        full_path_aws = save.save_image_s3(full_path, "{}/{}".format(aws_path, full_path))
 
-        time.sleep(10)
+        tif_path_aws = save.move_tif(im.key, "{}/{}".format(aws_path, im.key))
+
+        print(mod_path_aws)
+        print(full_path_aws)
+        print(tif_path_aws)
 
         # hazard = Hazard(haz_id, haz_name, HazardType.VOLCANO, Location(LatLong(center[0], center[1])), Date(img_date))
         # sat_id = SatelliteEnum.from_string(sat_name)
